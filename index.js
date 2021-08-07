01014ec6aaebfc66d3ecfa1990788363718828c5
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-08-06 20:33:59 -0400

    commit tree e74d1490782c4c817ad5b89b07aa89a2f4105f61

--- index.js
+++ index.js
@@ -7,7 +7,40 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
+
 /* End Given Code, don't edit above here...*/
+
+
+function greet(the_time) {
+  let this_time = the_time.split(":");
+ let hours = this_time[0];
+  let the_hour = parseInt(hours)
+
+  if (the_hour < 12) {
+    return "Good Morning";
+  }
+
+    // else if ((this_time >= 1200) && (this_time <= 1700)) {
+    //   return "Good Afternoon";
+    // }
+
+      else if (the_hour > 17 ) {
+      return "Good Evening";
+    }
+
+    else {
+      return "Good Afternoon";
+    }
+
+  
+}
+
+function displayMessage(message) {
+ const elem = document.getElementById("greeting")
+elem.textContent = message
+
+}
+
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
